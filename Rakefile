9d93884247acde82c61beec218ee9973710be259
Yuto Hayamizu <y.hayamizu@gmail.com>
2015-01-31 01:31:06 +0900

    introduced foundation

--- Rakefile
+++ Rakefile
@@ -1,2 +1,8 @@
 require "bundler/gem_tasks"
 
+task :assets do
+  Dir.chdir("assets") do
+    sh "bundle exec compass compile"
+  end
+end
+
--- lib/catscope/app.rb
+++ lib/catscope/app.rb
@@ -1,10 +1,33 @@
 # coding: utf-8
 
 require 'sinatra/base'
+require 'sinatra/assetpack'
 
 module Catscope
 
 class App < Sinatra::Base
+  register Sinatra::AssetPack
+
+  assets do
+    serve '/assets/js', from: File.expand_path('../../../assets/js', __FILE__)
+    serve '/assets/bower_components', from: File.expand_path('../../../assets/bower_components', __FILE__)
+
+    js :modernizr, [
+                    '/assets/bower_components/modernizr/modernizr.js',
+                   ]
+
+    js :libs, [
+               '/assets/bower_components/jquery/dist/jquery.js',
+               '/assets/bower_components/foundation/js/foundation.js'
+              ]
+
+    js :application, [
+                      '/assets/js/app.js'
+                     ]
+
+    js_compression :jsmin
+  end
+
   get('/') do
     erb :index
   end
