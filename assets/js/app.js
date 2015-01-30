87222380053ce29ad3a274585e6a42b26746a9e7
Yuto Hayamizu <y.hayamizu@gmail.com>
2015-01-31 04:02:39 +0900

    implemented file list view

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,44 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation();
+
+function filelist_expand_dir(entryElem, container) {
+    console.debug(entryElem, container);
+    $.getJSON("/api/lsdir" + entryElem.dataset.path,
+	      function(jsonData, status, jqxhr) {
+		  container.empty()
+		  for (var i = 0; i < jsonData.length; i++) {
+		      var entry = jsonData[i];
+		      var elem = $('<li>', {class: "js-filelist-" + entry.type});
+		      var child_ul = $('<ul>');
+		      var anch = $('<a>', {id: entry.id,
+					   "data-path": entry.path,
+					   "data-type": entry.type,
+					   "data-name": entry.name,
+					   text: entry.name,
+					   class: "js-filelist-" + entry.type,
+					   click: function(e){
+					       console.debug(e);
+					       console.debug(e.target.nextSibling);
+					       if (e.target.dataset.type == "dir") {
+						   filelist_expand_dir(e.target, $(e.target.nextSibling));
+					       } else {
+						   filelist_open_file(e.target.dataset.path);
+					       }
+					   }});
+		      elem.append(anch);
+		      elem.append(child_ul);
+		      container.append(elem);
+		  }
+	      });
+}
+
+function filelist_open_file(path) {
+    window.open('/file' + path, '_blank');
+}
+
+(function(){
+    var filelist = $("#js-filelist");
+
+    filelist_expand_dir(filelist[0], filelist);
+})();
--- lib/catscope/app.rb
+++ lib/catscope/app.rb
@@ -2,14 +2,19 @@
 
 require 'sinatra/base'
 require 'sinatra/assetpack'
+require 'pathname'
+require 'json'
 
 module Catscope
 
 class App < Sinatra::Base
+  TOP_DIR = Pathname.new(Dir.pwd).realpath
   register Sinatra::AssetPack
 
   assets do
+    serve '/assets/img', from: File.expand_path('../../../assets/img', __FILE__)
     serve '/assets/js', from: File.expand_path('../../../assets/js', __FILE__)
+    serve '/assets/stylesheets', from: File.expand_path('../../../assets/stylesheets', __FILE__)
     serve '/assets/bower_components', from: File.expand_path('../../../assets/bower_components', __FILE__)
 
     js :modernizr, [
@@ -30,6 +35,56 @@ class App < Sinatra::Base
 
   get('/') do
     erb :index
+  end
+
+  get('/file/*') do
+    path = File.expand_path(params[:splat][0].gsub(/^\//, ""), TOP_DIR.to_s)
+    name = File.basename(path)
+    if name =~ /\.([a-z0-9]+)$/
+      ext = $~[1]
+    else
+      ext = nil
+    end
+
+    case ext
+    when /^jpe?g$/i
+      content_type "image/jpeg"
+    when /^png$/i
+      content_type "image/png"
+    else
+      content_type "text/plain"
+    end
+
+    File.open(path)
+  end
+
+  # API for filelist
+  get('/api/lsdir/*') do
+    content_type "text/json"
+
+    path = File.expand_path(params[:splat][0].gsub(/^\//, ""), TOP_DIR.to_s)
+
+    entries = Dir.entries(path).select do |name|
+      ![".", ".."].include?(name)
+    end.map do |name|
+      Dir.chdir(path) do
+        entry_pathname = Pathname.new(File.expand_path(name, path))
+        entry = {
+          :name => name,
+          :path => "/" + entry_pathname.relative_path_from(TOP_DIR).to_s
+        }
+        if File.directory?(name)
+          entry[:type] = :dir
+        elsif File.file?(name)
+          entry[:type] = :file
+        end
+        entry[:id] = Digest::MD5.hexdigest(entry[:path])
+
+        entry
+      end
+    end
+
+    entries.to_json
   end
 end
 
