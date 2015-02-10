e3e50941233c296432bb15e6bdff1659360cca4f
Yuto Hayamizu <y.hayamizu@gmail.com>
2015-02-11 00:12:29 +0900

    make some rack option configurable from command line option

--- lib/catscope/cli.rb
+++ lib/catscope/cli.rb
@@ -13,10 +13,30 @@ class CLI
 
   def setup_parser()
     @parser = OptionParser.new
+
+    @bind = '127.0.0.1'
+    @parser.on('-o', '--bind ADDRESS', "IP address to bind (default: #{@bind})") do |addr|
+      @bind = addr
+    end
+
+    @port = 4567
+    @parser.on('-p', '--port PORT', "Port to listen on (default: #{@port})") do |port|
+      @port = Integer(port)
+    end
+
+    @environment = "production"
+    @parser.on('-e', '--env ENV', "Rack environment (default: #{@environment})") do |env|
+      @environment = env
+    end
   end
 
   def run(argv)
     @parser.parse!(argv)
+
+    App.set :environment, @environment
+    App.set :bind, @bind
+    App.set :port, @port
+
     App.run!
     true
   end
